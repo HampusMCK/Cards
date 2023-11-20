6acd7c945a5c06448d38949b972e52a303c55303
Hampus Eriksson <Hampus.Eriksson@elev.ga.ntig.se>
2023-11-20 12:24:01 +0100

    Cards Class, Shuffle func

---
+++ Cards/Program.cs
@@ -0,0 +1,37 @@
+﻿Cards c = new Cards();
+int n = 0;
+string co = "";
+string fo = "";
+
+for (int s = 0; s < 4; s++)
+{
+    n = 0;
+    if (s == 0)
+    {
+        co = "Red";
+        fo = "Heart";
+    }
+    else if (s == 1)
+        fo = "Diamond";
+    else if (s == 2)
+    {
+        co = "Black";
+        fo = "Club";
+    }
+    else
+        fo = "Spade";
+    for (int v = 2; v < 15; v++)
+    {
+        c.cards.Add(new Cards { value = v, color = co, form = fo, name = c.names[n] });
+        n++;
+    }
+}
+
+c.Shuffle(c.cards);
+
+for (int i = 0; i < c.cards.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {c.cards[i].value}");
+}
+
+Console.ReadLine();
